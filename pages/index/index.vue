a877e76d8f5c5adc27c7ecacfb2d734fe842cea4
sintrb <sintrb@gmail.com>
2021-05-13 12:30:33 +0800

    1.0.0

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,10 +1,3 @@
-# uniSignature
-用于uni-app的签名组件，支持H5、小程序、APP，可导出svg矢量图片。
-
-可以使用v-model进行绑定，或者通过方法.getSyncSignature()调用并获取数据。
-
-### 使用
-```html
 <template>
 	<view class="content" style="padding: 10rpx;">
 		<view style="border: 1rpx dashed #555555;">
@@ -32,6 +25,7 @@
 		methods: {
 			async startSign() {
 				let s = await this.$refs.sig.getSyncSignature();
+				console.log('组件版本', this.$refs.sig.VERSION);
 				console.log('签名数据', s);
 			}
 		}
@@ -40,12 +34,3 @@
 
 <style>
 </style>
-
-```
-
-
-### 运行截图
-
-![图片1](https://ishop-static-qn.inruan.com/Fqz4jBDVJEq5L3rNQ9BQnt7qxU6W.png)
-![图片2](https://ishop-static-qn.inruan.com/Fi23tBj4E4TZfd8JxIKVBasUUoes.png)
-![图片3](https://ishop-static-qn.inruan.com/Fqb3o3dG_Zpv063CPow97AiWeT90.png)
